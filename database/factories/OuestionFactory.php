d253b8d09baadc12e65df437a0ec2eaaa97b19f2
mmskazak <mmskazak@gmail.com>
2022-10-03 15:57:54 +0300

    Добавлена фабрика и миграция для таблицы вопросов.

---
+++ database/factories/OuestionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class OuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->title,
+            'text' => fake()->text(200),
+            'user_id' => fake()->numberBetween(1,10),
+        ];
+    }
+}
--- database/migrations/2022_10_03_115030_create_questions_table.php
+++ database/migrations/2022_10_03_115030_create_questions_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title',255);
             $table->text('text');
+            $table->integer('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
