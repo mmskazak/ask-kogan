d2201c61026c505e8c14a615ddef52283f69f362
mmskazak <mmskazak@gmail.com>
2022-10-11 17:54:16 +0300

    написал тест на голосование, но ненаписал логику под него, тесст пока падает

---
+++ tests/Feature/VotingTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VotingTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_voting_on_question()
+    {
+         $user = User::factory()->create();
+         $question  = Question::factory()->create([
+            'user_id' => $user->id
+         ]);
+
+        $this->assertDatabaseHas('questions', [
+            'title' => $question->title,
+            'text' =>  $question->text,
+            'user_id' => $user->id
+        ]);
+
+         $resultBool = $user->vote($question);
+
+         $this->assertTrue($resultBool);
+
+         $this->assertDatabaseHas('voting', [
+            'user_id' => $user->id,
+            'question_id' =>  $question->id,
+        ]);
+    }
+}
