1c4d99478f4138a3986f9ca1a7fbc98952edfa6e
mmskazak <mmskazak@gmail.com>
2022-10-12 17:12:22 +0300

    миграция, добавление текстового поля role в таблицу users, а так же миграция которя добаваляет поле moderation в таблицу questions

--- database/migrations/2022_10_12_134713_add_field_moderation_to_questions_table.php
+++ database/migrations/2022_10_12_134713_add_field_moderation_to_questions_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user');
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('moderation')->after('text');
         });
     }
 
@@ -25,8 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('moderation');
         });
     }
 };
